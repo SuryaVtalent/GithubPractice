498c1922c6d7ec2f4a1e8129e0f4c9a5e61ceea2
SuryaVtalent <suryaa.vtalent@gmail.com>
2024-07-26 12:39:43 +0530

    This is my Angular Project for Practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"",component:HomeComponent,pathMatch:'full'},  
+{path:"home",component:HomeComponent},
+{path:"login",component:LoginComponent},
+{path:"register",component:RegisterComponent},
+{path:"contact",component:ContactComponent},
+{path:"about",component:AboutComponent},
+{path:"**",component:HomeComponent,pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
